358e74be4134bd1e162c04a9dc11945ca7703fec
Peter Wong <peter.wong@travelex.com>
2016-10-16 09:39:49 -0700

    Country service

--- country/src/Main.hs
+++ country/src/Main.hs
@@ -2,7 +2,6 @@
 module Main where
 
 import           Data.Aeson
-import           Data.ByteString.Internal
 import           Snap.Core
 import           Snap.Http.Server
 import           CountryService
@@ -14,9 +13,4 @@ site :: Snap()
 site = route [ ("country", method GET countryHandler) ]
 
 countryHandler :: Snap()
-countryHandler = getQueryParam "target" >>= writeLBS.encode.countries.maybe Source toTarget
-
-toTarget :: ByteString -> Target
-toTarget "source"      = Source
-toTarget "destination" = Destination
-toTarget _             = error "Unrecognised target"
+countryHandler = getQueryParam "target" >>= writeLBS.encode.targetToCountries.maybe Source target
